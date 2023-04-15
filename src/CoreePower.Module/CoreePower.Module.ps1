059be375a6da3a0e01e3e508c6f55700add4a1a4
Carsten Riedel <carstenriedel@outlook.com>
2023-04-15 22:12:08 +0200

    Publish CoreePower.Module.psm1 0.0.0.35

--- src/CoreePower.Module/CoreePower.Module.ps1
+++ src/CoreePower.Module/CoreePower.Module.ps1
@@ -165,7 +165,7 @@ function PublishModule2 {
         if ($executable) {
             Write-Host "Git executable found at $($executable.Source) add, all commit and push"
             &git -C "$Path" add -A
-            &git -C "$Path" commit -m "Publish $($Data.RootModule) $($Data.ModuleVersion)" 
+            &git -C "$Path" commit -m "Publish $([System.IO.Path]::GetFileNameWithoutExtension($Data.RootModule)) $($Data.ModuleVersion)" 
             &git -C "$Path" push 
         }
         else {
